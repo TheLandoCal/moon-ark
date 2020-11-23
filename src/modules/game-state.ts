50a106c8de1687c153e47bc2b698c9a084fcdf69
Lando Toothman <landortoothman@gmail.com>
2020-11-22 23:56:17 -0500

    feat: Add instruction text and navigation

--- src/modules/game-state.ts
+++ src/modules/game-state.ts
@@ -7,4 +7,80 @@ export const state: any = {
     height: 800,
     backgroundColor: 0x000000,
   },
+  buttons: [],
+  instructions: [
+    {
+      page: 1,
+      text: 'You have one shot to escape the crumbling earth and make it to the moon.',
+    },
+    {
+      page: 2,
+      text: `Moon Ark is a single player game where you are trying to find a route off of Earth with the necessary supplies and make it to the Moon on the "Ark" before the Earth crumbles underneath you.
+      
+The Earth is not making things easy for you by destroying potential launch sites, evaporating precious resources, and sabotaging any routes you have may have planned for escape.
+
+The deck contains of 43 city cards that indicate places to gather materials to help you escape the planet on the Moon Ark.`,
+    },
+    {
+      page: 3,
+      text: `The #0 card, your first City, is placed in the center of the play area. This is the beginning of your escape route.
+
+You will be given City cards #1, #2, #3, and #42 (the cities you were able to get to before the Earth reached its critical stage, and your final launch site). The remaining City cards are sorted into three decks: Regional, National, Global
+
+You will able about to draw 5 additional City cards to start:
+- Local deck: Draw 3 City cards
+- National deck: Draw 2 City cards
+- Global deck: Draw 0 City cards`,
+    },
+    {
+      page: 4,
+      text: `Each City card has 2 numbers: a Route Stop number (#0 - #42) and a Boost number (marked by packages).
+
+You use cards by either placing them as Route Stops or by forfeiting them to boost your speed. The Earth can destroy deck cards at any time to reduce the number of possible Route Stops you can use to escape.`,
+    },
+    {
+      page: 5,
+      text: `You can use City cards to advance along your route. You are allowed to advance to a City if it is no more than 3 higher than the number of the previous City placed. You cannot place a new City whose number is lower than the previous City.
+
+In order to advance more than 3 numbers in a turn, you may choose to sacrifice a City's resources to gain a boost to escape the Earth faster. The number ON these cards are not displayed, just how many were sacrified. The amount of the boost is determined by the number of packages on the boost cards.
+
+You are not required to use all of the boost obtained from the sacrificed (in case you want to use it to through looters off your trail).`,
+    },
+    {
+      page: 6,
+      text: `Example 1: Normal Advancement
+
+The previous City is #6. You may chose to play the #7, #8, or #9 City card. You cannot play the #4 City card because it is lower than your previous City and you cannot play the #10 City card because it is more than 3 higher than your previous City.
+
+Example 2: Boost Advancement
+
+The previous City is #6. You may chose to play the #11 City card by sacrificing the #4 City card (which has 2 packages) boost your advancement by +2. You can also play the #10, #9, #8, or #7 City card because you are not required to use all of your boost.`,
+    },
+    {
+      page: 7,
+      text: `You may choose to pass your turn and play no City cards. This lets you build up City cards in your hand for later use, but you also risk the Earth sabotaging your future escape routes.`,
+    },
+    {
+      page: 8,
+      text: `During its turn, the Earth can destroy any City card left in any of the 3 decks. The Earth's destruction starts in your local area and spreads throughout the globe. As you advance into cards from the other decks, the Earth's destruction path will grow and start to destroy Cities in those decks as well.`,
+    },
+    {
+      page: 9,
+      text: `First Turns
+
+- You: Place 1 or 2 new Route Stops
+- Earth: Destroy 2 Cities from any deck
+
+Subsequent Turns
+
+- You: Draw 1 Card from any deck. Place a new Route Stop or Pass
+- Earth: Destroy 1 City from any deck`,
+    },
+    {
+      page: 10,
+      text: `You win by playing City cards until you can ultimately play the #42 City card and escape on the Ark.
+
+You lose if the Earth is able to destroy all potential paths forward and make it impossible for you to find an escape route.`,
+    },
+  ],
 };
--- src/scenes/instructions.scene.ts
+++ src/scenes/instructions.scene.ts
@@ -22,30 +22,126 @@ export default class InstructionsScene extends Phaser.Scene {
    * Pre-load components in Instructions Scene.
    */
   preload() {
-    this.load.addFile(new WebFont(this.load, 'Roboto'));
+    this.load.addFile(new WebFont(this.load, ['Amatic SC', 'Roboto']));
   }
 
   /**
    * Create components in Instructions Scene.
    */
   create(): void {
+    this.loadTitle();
+    this.displayInstruction();
+    this.loadNavigation();
+    this.loadStartButton();
+  }
+
+  /**
+   * Update components in Instructions Scene.
+   */
+  update(): void {}
+
+  private loadTitle(): void {
     const title = new Text(
       this,
-      this.state.screen.width * (1 / 2),
-      this.state.screen.height * (1 / 2),
+      this.state.screen.width * 0.5,
+      this.state.screen.height * 0.15,
       'How To Play',
       {
-        fontFamily: 'Roboto',
-        fontSize: '36px',
+        fontFamily: '"Amatic SC"',
+        fontSize: '48px',
         align: 'center',
       }
     );
     title.setOrigin(0.5);
+    title.load();
+  }
 
-    const button = new Button(
+  private displayInstruction(forward = true): void {
+    const instructionStyle = {
+      fontFamily: 'Roboto',
+      fontSize: '24px',
+      align: 'center',
+      wordWrap: { width: this.state.screen.width * 0.75 },
+      lineSpacing: 8,
+    };
+
+    const nextInstructionMetadata = this.retrieveNextInstructionMetadata(forward);
+
+    if (this.state.currentInstruction) {
+      this.state.currentInstruction.destroy();
+    }
+
+    if (nextInstructionMetadata) {
+      const nextInstruction = new Text(
+        this,
+        this.state.screen.width * 0.5,
+        this.state.screen.height * 0.45,
+        nextInstructionMetadata.text,
+        instructionStyle
+      );
+
+      nextInstruction.setOrigin(0.5);
+      nextInstruction.load();
+
+      this.state.currentInstruction = nextInstruction;
+    } else {
+      this.state.currentInstruction = undefined;
+    }
+  }
+
+  private loadNavigation(): void {
+    let nextButton = this.state.buttons.find((b: any) => b.name === 'nextButton');
+
+    if (!nextButton) {
+      nextButton = {
+        name: 'nextButton',
+        element: new Button(
+          this,
+          this.state.screen.width * 0.5,
+          this.state.screen.height * 0.8,
+          'next-button',
+          'Next',
+          () => this.displayInstruction(),
+          'warning',
+          false,
+          false,
+          'lg'
+        ),
+      };
+    }
+
+    nextButton.element.load();
+    this.state.buttons.push(nextButton);
+  }
+
+  private retrieveNextInstructionMetadata(forward = true): any {
+    const currentInstruction = this.state.currentInstruction;
+
+    if (currentInstruction) {
+      const idx = this.state.instructions.findIndex((i: any) => i.text === currentInstruction.text);
+
+      if (idx < this.state.instructions.length - 1) {
+        if (idx == this.state.instructions.length - 2) {
+          let nextButton = this.state.buttons.find((b: any) => b.name === 'nextButton');
+          nextButton.element.node.onclick = null;
+          nextButton.element.node.classList.remove('btn-warning');
+          nextButton.element.node.classList.add('btn-outline-warning');
+          nextButton.element.node.disabled = true;
+        }
+        return this.state.instructions[idx + (forward ? 1 : -1)];
+      }
+
+      return null;
+    }
+
+    return this.state.instructions[0];
+  }
+
+  private loadStartButton(): void {
+    const startButton = new Button(
       this,
-      this.state.screen.width * (1 / 2),
-      this.state.screen.height * (4 / 5),
+      this.state.screen.width * 0.5,
+      this.state.screen.height * 0.9,
       'start-button',
       'Start',
       () => advance(this.scene, 'GameScene'),
@@ -54,13 +150,6 @@ export default class InstructionsScene extends Phaser.Scene {
       false,
       'lg'
     );
-
-    title.load();
-    button.load();
+    startButton.load();
   }
-
-  /**
-   * Update components in Instructions Scene.
-   */
-  update(): void {}
 }
